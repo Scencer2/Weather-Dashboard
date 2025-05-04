1c63cfe5cf5ac67ddb9d71b46228ef50df475f4a
Scencer2 <Scencer2@yahoo.com>
2025-05-03 21:58:19 -0700

    idk

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,11 +16,11 @@ const __dirname = path.dirname(__filename);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 app.use('/api', routes);
 app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+   res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
         
 
